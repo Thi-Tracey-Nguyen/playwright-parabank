5ecd1fae9c651aee8136b24cee17270f4db5cf62
Thi-Tracey_Nguyen <traceynguyen.1988@gmail.com>
2023-11-14 17:22:53 +1100

    Added register page object model

---
+++ pages/register-page.ts
@@ -0,0 +1,32 @@
+import { type Locator, type Page } from '@playwright/test';
+
+export class RegisterPage {
+  readonly page: Page
+  readonly firstName: Locator
+  readonly lastName: Locator
+  readonly address: Locator
+  readonly city: Locator
+  readonly state: Locator
+  readonly zipCode: Locator
+  readonly phone: Locator
+  readonly ssn: Locator
+  readonly username: Locator
+  readonly password: Locator
+  readonly confirm: Locator
+
+
+  constructor(page: Page) {
+    this.page = page
+    this.firstName = page.locator('id=customer.firstName')
+    this.lastName = page.locator('id=customer.lastName')
+    this.address = page.locator('id=customer.address.street')
+    this.city = page.locator('id=customer.address.city')
+    this.state = page.locator('id=customer.address.state')
+    this.zipCode = page.locator('id=customer.address.zipCode')
+    this.phone = page.locator('id=customer.phoneNumber')
+    this.ssn = page.locator('id=customer.ssn')
+    this.username = page.locator('id=customer.username')
+    this.password = page.locator('id=customer.password')
+    this.confirm = page.locator('id=customer.repeatedPassword')
+  }
+}
